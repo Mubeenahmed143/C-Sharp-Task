14a1a65b6693a3f533102b0d011c0c94d22a9b62
Mubeenahmed143 <ahmedsheikhmubeen395@gmail.com>
2024-07-02 22:22:20 +0500

    Assignment

---
+++ Class_Task/Book.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Class_Task
+{
+    internal class Book
+    {
+
+        private string author;
+        private string title;
+        private int isbn;
+        private int check_out;
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public int ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+
+        public int Check_out
+        {
+            get { return check_out; }
+            set { check_out = value; }
+        }
+
+        public void Checkedout()
+        {
+            Console.WriteLine("Author: {0}", Author);
+            Console.WriteLine("Title: {0}", Title);
+            Console.WriteLine("ISBN: {0}", ISBN);
+            Console.WriteLine("Checked out: {0}", Check_out);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Author = "John Doe";
+            book.Title = "Sample Book";
+            book.ISBN = 1234567890;
+            book.Check_out = 1;
+
+            book.Checkedout();
+        }
+    }
+}
---
+++ Class_Task/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Class_Task;
+
+Console.WriteLine("Hello, World!");
+
+
+Book b1 = new Book();
+b1.Checkedout();
+console.WriteLine(b1);
+
+
+//static void Main(string[] args)
+//{
+//    Book book = new Book();
+//    book.Author = "John Doe";
+//    book.Title = "Sample Book";
+//    book.ISBN = 1234567890;
+//    book.Check_out = 1;
+
+//    book.Checkedout();
+//}
+
+//console.WriteLine(Main(args));
